0649d4857cf2d62c3d5ed0dd1a81b334ad73cc13
Georgi Komov <69540742+gkomov123@users.noreply.github.com>
2025-11-16 06:32:10 +0200

    add: Core functions for data entry

---
+++ data_entry.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+def get_date(prompt, allow_default=False):
+    pass
+
+
+def get_amount():
+    pass
+
+
+def get_category():
+    pass
+
+
+def get_description():
+    pass
